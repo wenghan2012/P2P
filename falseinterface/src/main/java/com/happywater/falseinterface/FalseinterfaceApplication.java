44a6361cdaab372ae7ab2f9ddb43994971f4088d
wenghan2012 <39816366+wenghan2012@users.noreply.github.com>
2018-09-20 15:15:10 +0800

    聚金融-假数据-1.0

---
+++ falseinterface/src/main/java/com/happywater/falseinterface/FalseinterfaceApplication.java
@@ -0,0 +1,11 @@
+package com.happywater.falseinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalseinterfaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FalseinterfaceApplication.class, args);
+    }
+}
